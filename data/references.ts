a7b3cf518701d1fad07a8f3ac41ad221efd566ae
cvegabello <carlosvegabello@gmail.com>
2026-06-25 21:29:52 -0400

    feat: add Chase Campbell reference and avatar

--- data/references.ts
+++ data/references.ts
@@ -10,7 +10,7 @@ export const referencesData = {
       id: 1,
       name: "Kylie Gibbs",
       role: "School Counselor at Schalmont High School",
-      email: "kgibbs@schalmont.net", 
+      email: "kgibbs@schalmont.net",
       avatar: "/img/kylie-gibbs-avatar.png", // Foto de la persona
       quote:
         "Santiago is an exceptional student. He has a natural ability to not only succeed academically but thrive in college preparatory courses. His teachers and peers enjoy having him in class, he is respectful, kind, and conscientious of others. I have no doubt that he will do well wherever he chooses to study.",
@@ -21,7 +21,7 @@ export const referencesData = {
       id: 2,
       name: "Vito Urbano",
       role: "Head Varsity Soccer Coach at Schalmont High School",
-      email: "vitourbano@gmail.com", 
+      email: "vitourbano@gmail.com",
       avatar: "/img/vito-urbano-avatar.png",
       quote:
         "Santiago is an outstanding student-athlete. [Placeholder: We will update this quote as soon as Coach Urbano sends his official recommendation letter.]",
@@ -30,13 +30,24 @@ export const referencesData = {
     },
     {
       id: 3,
-      name: "Monica Dicocco",
-      role: "Spanish Teacher & Club Advisor",
+      name: "Mónica Muñiz DiCocco",
+      role: "Spanish Teacher & Spanish Club, Spanish Honor Society, and SADD Club Advisor at Schalmont High School",
       email: "mmdicocco@schalmont.net",
       avatar: "/img/monica-dicocco-santi.png",
       quote:
-        "Santiago has always shown great dedication and kindness. [Placeholder: We will update this quote as soon as Ms. Dicocco sends her official recommendation.]",
-      signature: "- Monica Dicocco",
+        "Having known Santi since he arrived from Colombia at age five without speaking much English, it has been a privilege to watch him transform into a brilliant scholar, Honor Society member, and Spanish Club officer. His exceptional work ethic shines through in everything he does, from his dedication on the soccer field to his enrollment in college level courses. He possesses a rare blend of resilience, intellect, and leadership that will undoubtedly make him an asset to any university campus.",
+      signature: "- Mónica Muñiz DiCocco",
+      pdfUrl: null,
+    },
+    {
+      id: 4,
+      name: "Chase Campbell",
+      role: "Global Studies Teacher at Schalmont High School",
+      email: "ccampbell@gmail.com",
+      avatar: "/img/chase-campbell-avatar.png",
+      quote:
+        "Santiago is an outstanding student and an amazing friend. [Placeholder: We will update this quote as soon as Mr. Campbell sends his official recommendation.]",
+      signature: "- Chase Campbell",
       pdfUrl: null,
     },
   ],
